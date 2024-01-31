3587b51f822e62cd44b15353fc24f7339aeddcee
Kelvi Isaj <kelvi.isaj@ynap.com>
2024-01-31 10:37:30 +0100

    price add

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -16,14 +16,13 @@ describe('API Pact test', () => {
   describe('retrieving a product', () => {
     test('ID 10 exists', async () => {
       // Arrange
-      const expectedProduct = {
-        id: '10',
-        type: 'CREDIT_CARD',
-        name: '28 Degrees'
-      };
+      // const expectedProduct = {
+      //   id: '10',
+      //   type: 'CREDIT_CARD',
+      //   name: '28 Degrees'
+      // };
 
-      // Uncomment to see this fail
-        //const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0}
+        const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0}
 
       mockProvider
         .given('a product with ID 10 exists')
